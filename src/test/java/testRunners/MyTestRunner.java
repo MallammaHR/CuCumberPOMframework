ff56f2ff56793b2e04b8cbdc415f487ea5f1e3ec
Mallammma H R <41098705+MallammaHR@users.noreply.github.com>
2024-02-27 22:09:36 -0600

    Update MyTestRunner.java

--- src/test/java/testRunners/MyTestRunner.java
+++ src/test/java/testRunners/MyTestRunner.java
@@ -1,17 +1,17 @@
 package testRunners;
 
-//import org.junit.runner.RunWith;
-//
-//import io.cucumber.junit.Cucumber;
-//import io.cucumber.junit.CucumberOptions;
-//
-//@RunWith(Cucumber.class)
-//@CucumberOptions(features = {"src/test/resource/com/features"},
-//glue ={"com.StepDefinations","AppHooks"},
-//plugin={"pretty"})
-//
-//public class MyTestRunner 
-//{
+import org.junit.runner.RunWith;
 
-//}
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = {"src/test/resource/com/features"},
+glue ={"com.StepDefinations","AppHooks"},
+plugin={"pretty"})
+
+public class MyTestRunner 
+{
+
+}
 
